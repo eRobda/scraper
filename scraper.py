770115e942d056d87a4355ff95721504399adc0f
eRobda <51453874+eRobda@users.noreply.github.com>
2022-12-26 17:59:07 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,19 +1,24 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from flask import Flask, request
 
-jmeno = input();
+app = Flask(__name__)
 
-driver = webdriver.Chrome()
+@app.route("/", methods=["POST"])
+def index():
+    jmeno = request.form["url"]
+    driver = webdriver.Chrome()
 
-url = 'https://prehrajto.cz/'
-driver.get(url)
+    url = 'https://prehrajto.cz/'
+    driver.get(url)
 
-driver.execute_script(f"document.getElementById('search-phrase').value = '{jmeno}';")
-driver.execute_script("document.querySelector('button').click();")
-driver.execute_script("document.querySelectorAll('a')[7].click();")
+    driver.execute_script(f"document.getElementById('search-phrase').value = '{jmeno}';")
+    driver.execute_script("document.querySelector('button').click();")
+    driver.execute_script("document.querySelectorAll('a')[7].click();")
 
-video_elements = driver.find_elements(By.TAG_NAME, 'video')
+    video_elements = driver.find_elements(By.TAG_NAME, 'video')
 
-video_src = video_elements[0].get_attribute('src')
+    video_src = video_elements[0].get_attribute('src')
 
-print(video_src)
+    return video_src
